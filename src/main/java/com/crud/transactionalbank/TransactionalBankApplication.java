c389bb741940535ac865b8c46854c632ffbbf040
Saikumar Adepu <saikumaraadepu1@gmail.com>
2024-11-29 13:07:03 +0530

    Initial State

---
+++ src/main/java/com/crud/transactionalbank/TransactionalBankApplication.java
@@ -0,0 +1,13 @@
+package com.crud.transactionalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalBankApplication.class, args);
+	}
+
+}
